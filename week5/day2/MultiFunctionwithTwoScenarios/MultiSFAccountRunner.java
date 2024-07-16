92d93aea0a712d91817df99cfb5268b96808f57e
Sharmila-1812 <sharmilasekar18@gmail.com>
2024-07-17 00:29:54 +0530

    Update MultiSFAccountRunner.java

--- week5/day2/MultiFunctionwithTwoScenarios/MultiSFAccountRunner.java
+++ week5/day2/MultiFunctionwithTwoScenarios/MultiSFAccountRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = {
-		"src/test/java/features/MultiSFCreateAccount.feature" }, glue = "StepDef", publish = true, monochrome = true)
+		"src/test/java/features/MultiSFAccount.feature" }, glue = "StepDef", publish = true, monochrome = true)
 public class MultiSFAccountRunner extends AbstractTestNGCucumberTests {
 
 }
